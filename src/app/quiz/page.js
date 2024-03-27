866ea198feb0508777e3474f049af47303b23041
DanielBoye <danielboye888@gmail.com>
2024-03-27 23:51:15 +0100

    add use client

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,9 +1,14 @@
-export default function Quiz() {
-    return (
-        <>
-            <div>
-                <h1 className="text-slate-50">Quiz Page</h1>
-            </div>
-        </>
-    );
-}
+"use client";
+
+import React from 'react';
+import Quiz from '@/components/Quiz';
+
+const QuizPage = () => {
+  return (
+    <div className="bg-black min-h-screen text-white p-5">
+      <Quiz />
+    </div>
+  );
+};
+
+export default QuizPage;
