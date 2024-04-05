23fed597f567418446fed55480d136fcdfb80a71
DanielBoye <danielboye888@gmail.com>
2024-04-06 01:13:32 +0200

    update text

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import clsx from "clsx";
 import { SignInIcon } from "@/components/icons/signIn";
@@ -8,72 +8,71 @@ import { Container } from "@/components/container/Container";
 import styles from "./index.module.css";
 import { ComponentProps, useCallback } from "react"; // Import useCallback
 
-
-
 function Feature({ title, description, className, ...props }) {
-  return (
-    <div className={clsx(className, styles.featuresFeature)} {...props}>
-      <h4 className={styles.featuresFeatureTitle}>{title}</h4>
-      <p className={styles.featuresFeatureDescription}>{description}</p>
-    </div>
-  );
+    return (
+        <div className={clsx(className, styles.featuresFeature)} {...props}>
+            <h4 className={styles.featuresFeatureTitle}>{title}</h4>
+            <p className={styles.featuresFeatureDescription}>{description}</p>
+        </div>
+    );
 }
 
 export default function Index() {
-  // Directly define the redirection inside the onClick
-  const redirectToGoogle = useCallback(() => {
-    // Redirect to Google
-    window.location.href = '/dashboard';
-  }, []);
+    // Directly define the redirection inside the onClick
+    const redirectToGoogle = useCallback(() => {
+        // Redirect to Google
+        window.location.href = "/dashboard";
+    }, []);
 
-  return (
-    <MarketingLayout>
-      <Container className={styles.sectionHeader}>
-        <div className={styles.heroInfo}>
-          <h1 className={styles.heroTitleRoot}>
-            Arbitrarian
-          </h1>
-          <p className={styles.heroLeadDashboard}>
-            Learn how to develop and deploy tokens from smart contracts
-            from a few clicks&nbsp;that deploys in seconds.
-          </p>
-        </div>
-        <div className={styles.heroActions}>
-          <Button icon={<SignInIcon />} onClick={redirectToGoogle}>
-            Connect Wallet
-          </Button>
-          <LinkButton
-            href="https://arbitrum.io/"
-            target="_blank"
-            variant="secondary"
-          >
-            Learn more
-          </LinkButton>
-        </div>
-      </Container>
-      <Container className={styles.sectionFeature}>
-        <h2 className={styles.sectionTitle}>Learn</h2>
-        <div className={styles.featuresGrid}>
-          <Feature
-            description={
-              <>
-                asdjaslfjasløkfjasøldkfjasløkdfjasløkdjfsl
-                lajsdaskljfaslkjfasdlkfjsadlfkjsadlfkjøasdfølk
-              </>
-            }
-            title="Web3 Security"
-          />
-          <Feature
-            description={
-              <>
-                Best practices followed, using a mixture of SSR and custom API
-                endpoints. Modify documents from both client and server.
-              </>
-            }
-            title="ERC20 Tokens"
-          />
-        </div>
-      </Container>
-    </MarketingLayout>
-  );
+    return (
+        <MarketingLayout>
+            <Container className={styles.sectionHeader}>
+                <div className={styles.heroInfo}>
+                    <h1 className={styles.heroTitleRoot}>Arbitrarian</h1>
+                    <p className={styles.heroLeadDashboard}>
+                        Learn about smart contract security and how to develop
+                        and deploy tokens from smart contracts from a few
+                        clicks&nbsp;that deploys in seconds.
+                    </p>
+                </div>
+                <div className={styles.heroActions}>
+                    <Button icon={<SignInIcon />} onClick={redirectToGoogle}>
+                        Connect Wallet
+                    </Button>
+                    <LinkButton
+                        href="https://arbitrum.io/"
+                        target="_blank"
+                        variant="secondary"
+                    >
+                        Learn more
+                    </LinkButton>
+                </div>
+            </Container>
+            <Container className={styles.sectionFeature}>
+                <h2 className={styles.sectionTitle}>Learn</h2>
+                <div className={styles.featuresGrid}>
+                    <Feature
+                        description={
+                            <>
+                                Enhance your understanding of blockchain
+                                technology and smart contract security through
+                                our comprehensive tests.
+                            </>
+                        }
+                        title="Web3 Security"
+                    />
+                    <Feature
+                        description={
+                            <>
+                                Easily deploy ERC20 tokens directly from your
+                                browser with our user-friendly interface with a
+                                few clicks, and learn how to interact with them.
+                            </>
+                        }
+                        title="ERC20 Tokens"
+                    />
+                </div>
+            </Container>
+        </MarketingLayout>
+    );
 }
